0dfa13a2e0681ee07a9376b5f8d4f8e3b40f933f
Mikhael <mick.dunaev@yandex.ru>
2018-11-11 15:00:37 +0300

    base

---
+++ app/src/main/java/ru/yandex/dunaev/mick/myanimation/animations/FlyThereAndBackAnimationActivity.kt
@@ -0,0 +1,10 @@
+package ru.yandex.dunaev.mick.myanimation.animations
+
+import android.view.View
+
+class FlyThereAndBackAnimationActivity: BaseActivity() {
+    override fun onStartAnimation(view: View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ app/src/main/java/ru/yandex/dunaev/mick/myanimation/animations/LaunchAndSpinViewPropertyAnimatorAnimationActivity.kt
@@ -0,0 +1,10 @@
+package ru.yandex.dunaev.mick.myanimation.animations
+
+import android.view.View
+
+class LaunchAndSpinViewPropertyAnimatorAnimationActivity: BaseActivity() {
+    override fun onStartAnimation(view: View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
